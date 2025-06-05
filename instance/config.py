24be5227df809f1d7decd6b5c12548064e33f8ca
Bryan DeFaria <90565676+defariabATWIT@users.noreply.github.com>
2025-06-05 19:27:11 -0400

    Base SoMi 2.0

---
+++ instance/config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_default_secret_key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://somiadmin:sweng2025@localhost/somidb'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAX_CONTENT_LENGTH = 8000000  # 8 MB file limit
+    UPLOAD_EXTENSIONS = ['.jpg', '.png', '.avif', '.HEIC']
+    UPLOAD_PATH = '/var/www/somi/uploads/'
